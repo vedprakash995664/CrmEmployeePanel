61d73c5a1495e56c5d1e4fa4dc81867098155d25
Ved Prakash <vedprakash995664@gmail.com>
2025-02-13 16:58:58 +0530

    Ok

--- src/main.jsx
+++ src/main.jsx
@@ -5,20 +5,20 @@ import App from './App.jsx'
 
 import 'react-toastify/dist/ReactToastify.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
-<<<<<<< HEAD
+
 import  store  from './Redux/Store.js'
 import 'primereact/resources/themes/lara-light-indigo/theme.css'; // Optional, choose a theme
 import 'primereact/resources/primereact.min.css'; // Core styles
 import 'primeicons/primeicons.css'; // Icons
 import 'primeflex/primeflex.css'; // For utility classes (optional)
 import { Provider } from 'react-redux';
-=======
+
 
 import 'primereact/resources/themes/lara-light-indigo/theme.css';
 import 'primereact/resources/primereact.min.css'; 
 import 'primeicons/primeicons.css';
 import 'primeflex/primeflex.css'; 
->>>>>>> 0d7293807b95022a9a847baf41a3fbf99e4f1c1d
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
