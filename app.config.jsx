36253e9cbba1bda25a3aceecada7d15d4e776756
Ved Prakash <vedprakash995664@gmail.com>
2025-04-28 12:02:30 +0530

    Done

--- app.config.jsx
+++
@@ -1,7 +0,0 @@
-export default {
-    name: "CRM Project",         // Human-friendly name
-    slug: "crmproject",          // Matches the "name" from package.json
-    version: "0.0.0",            // Matches the "version" from package.json
-    description: "A CRM dashboard built with React and CoreUI",
-  }
-  
